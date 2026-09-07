9807b87c39114f26cffc7b48dd9e029a4164cd76
Joseph Wolochow <58348869+joewolly@users.noreply.github.com>
2026-09-07 11:34:19 -0600

    Make Windows installer exit status deterministic

--- install.ps1
+++ install.ps1
@@ -5,5 +5,7 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$LASTEXITCODE = 0
 & (Join-Path $scriptDir 'pantheon.ps1') install
+if (-not $?) { exit 1 }
 exit $LASTEXITCODE
